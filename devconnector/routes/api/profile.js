0208cbaeb781b869f1033403c6eb395a6dba4534
Tom Mac <tomvmac@yahoo.com>
2019-04-10 10:41:55 -0400

    Added finder routes to profile

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -23,6 +23,10 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+        .populate(
+          'user',
+          ['name', 'avatar']
+        )
         .then(profile => {
             if (!profile){
                 errors.noprofile = 'There is no profile for this user';
@@ -31,6 +35,84 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
             res.json(profile);
         })
         .catch(err => res.status(404).json(err));
+});
+
+
+// @route      GET api/profile/handle/:handle
+// desc        Get user profile by handle
+// @access     public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({handle: req.params.handle})
+    .populate(
+      'user',
+      ['name', 'avatar']
+    )
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => {
+      errors.noprofile = 'There is no profile for this user';
+      res.status(404).json(errors)
+    });
+  
+});
+
+
+// @route      GET api/profile/user/:user_id
+// desc        Get user profile by user id
+// @access     public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({user: req.params.user_id})
+    .populate(
+      'user',
+      ['name', 'avatar']
+    )
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => {
+      errors.noprofile = 'There is no profile for this user';
+      res.status(404).json(errors)
+    });
+  
+});
+
+
+// @route      GET api/profile/all
+// desc        Get all user profiles 
+// @access     public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find({})
+    .populate(
+      'user',
+      ['name', 'avatar']
+    )
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => {
+      errors.noprofile = 'There are no profiles';
+      res.status(404).json(errors)
+    });
+  
 });
 
 
