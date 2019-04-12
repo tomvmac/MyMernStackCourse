9dfaaeae88af15cd0ef091bd07365f159e70da6c
Tom Mac <tomvmac@yahoo.com>
2019-04-11 20:13:39 -0400

    Updated app with Login

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-// Router 
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+// Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// JWT
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
 // Devconnector Components
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import './App.css';
+import "./App.css";
+
+// Check for Token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
+      <Provider store={store}>
         <Router>
           <div className="App">
             <Navbar />
