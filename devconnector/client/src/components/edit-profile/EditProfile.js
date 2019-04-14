c0be5484fae1942a3c71fe387efc20c7096052ee
Tom Mac <tomvmac@yahoo.com>
2019-04-14 17:09:41 -0400

    added go back on edit profile

--- devconnector/client/src/components/edit-profile/EditProfile.js
+++ devconnector/client/src/components/edit-profile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // Actions
@@ -199,6 +199,9 @@ class EditProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
               <h1 className="display-4 text-center">Edit Your Profile</h1>
               <p className="lead text-center">
                 Let's get some information to make your profile stand out
