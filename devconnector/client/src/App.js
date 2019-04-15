b858f31264522a55abfe8ba0bc36c10f7ff0aa82
Tom Mac <tomvmac@yahoo.com>
2019-04-15 16:42:51 -0400

    created profile pages

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -25,11 +25,13 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
 
@@ -66,6 +68,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
                 <PrivateRoute
@@ -89,6 +92,7 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
